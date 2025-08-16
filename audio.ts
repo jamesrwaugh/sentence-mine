32eb4fee79f7f2138c40df35db71bf84ee000698
James <james.waugh.r@gmail.com>
2025-08-16 11:53:56 -0400

    Add in jpod101 alternate download url, from Yomitan inspection on how they got it

--- audio.ts
+++ audio.ts
@@ -24,25 +24,6 @@ function isStringEntirelyKana(str: string) {
   return true;
 }
 
-async function _getInfoJpod101(term: string, reading: string) {
-  if (reading === term && isStringEntirelyKana(term)) {
-    reading = term;
-    term = "";
-  }
-
-  const params = new URLSearchParams();
-  if (term.length > 0) {
-    params.set("kanji", term);
-  }
-  if (reading.length > 0) {
-    params.set("kana", reading);
-  }
-
-  const url = `https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?${params.toString()}`;
-
-  return [{ type: "url", url }];
-}
-
 const LanguagePodInvalidAudioHash =
   "ae6398b5a27bc8c0a771df6c907ade794be15518174773c58c7c7ddd17098906";
 
@@ -57,21 +38,120 @@ async function arrayBufferDigest(arrayBuffer: ArrayBuffer) {
   return digest;
 }
 
-export async function tryDownloadJpod101Audio(
+type DownloadResult =
+  | {
+      url: string;
+      audio: ArrayBuffer;
+      valid: true;
+    }
+  | {
+      url: undefined;
+      audio: undefined;
+      valid: false;
+    };
+
+async function _getInfoJpod101(
+  term: string,
+  reading: string
+): Promise<DownloadResult> {
+  if (reading === term && isStringEntirelyKana(term)) {
+    reading = term;
+    term = "";
+  }
+
+  const params = new URLSearchParams();
+
+  if (term.length > 0) {
+    params.set("kanji", term);
+  }
+
+  if (reading.length > 0) {
+    params.set("kana", reading);
+  }
+
+  const url = `https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?${params.toString()}`;
+
+  const response = await fetch(url);
+  const audio = await response.arrayBuffer();
+  const digest = await arrayBufferDigest(audio);
+
+  if (digest === LanguagePodInvalidAudioHash) {
+    return {
+      url: undefined,
+      audio: undefined,
+      valid: false,
+    };
+  }
+
+  return {
+    url,
+    audio,
+    valid: true,
+  };
+}
+
+async function _getInfoJpod101Alt(term: string): Promise<DownloadResult> {
+  const params = new URLSearchParams();
+  params.append("post", "dictionary_reference");
+  params.append("match_type", "exact");
+  params.append("search_query", term);
+
+  const requestOptions = {
+    method: "POST",
+    body: params,
+    redirect: "follow" as const,
+  };
+
+  const response = await fetch(
+    "https://www.japanesepod101.com/learningcenter/reference/dictionary_post",
+    requestOptions
+  );
+
+  /*
+    Returns a page with:
+
+    <audio preload="none" controls>
+      <source src="https://d1pra95f92lrn3.cloudfront.net/audio/375292.mp3" type="audio/mp3">
+    </audio>
+  */
+
+  const text = await response.text();
+
+  const audioUrl = text.match(/<source src="([^"]+)" type="audio\/mp3">/)?.[1];
+
+  if (!audioUrl) {
+    return {
+      url: undefined,
+      audio: undefined,
+      valid: false,
+    };
+  }
+
+  const audio = await fetch(audioUrl).then((r) => r.arrayBuffer());
+
+  return {
+    url: audioUrl,
+    audio,
+    valid: true,
+  };
+}
+
+const methods = [_getInfoJpod101, _getInfoJpod101Alt];
+
+export async function tryDownloadTermAudio(
   term: string,
   reading: string
 ): Promise<string | undefined> {
-  const info = await _getInfoJpod101(term, reading);
-  const response = await fetch(info[0]!.url);
-  const audio = await response.arrayBuffer();
-  // Save to file
-  const digest = await arrayBufferDigest(audio);
-  if (digest === LanguagePodInvalidAudioHash) {
-    return undefined;
+  for (const method of methods) {
+    const info = await method(term, reading);
+    if (info.valid) {
+      const filename = `audio-temp/${term}.mp3`;
+      await Bun.write(filename, info.audio);
+      return filename;
+    }
   }
-  const filename = `audio-temp/${term}.mp3`;
-  await Bun.write(filename, audio);
-  return filename;
+
+  return undefined;
 }
 
 // const f = await tryDownloadJpod101Audio("同意", "ど");
